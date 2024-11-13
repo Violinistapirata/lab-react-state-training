54839eabfca18769e863301da5dab1cc5fc3543a
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-13 19:54:55 +0100

    clear button styles

--- src/Components/LikeButton.jsx
+++ src/Components/LikeButton.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 function LikeButton() {
     const [likes, setLikes] = useState(0);
 
-  return <button className="like-button" onClick={() => setLikes(likes +1)}>{likes + ' Likes'}</button>;
+  return <button onClick={() => setLikes(likes +1)}>{likes + ' Likes'}</button>;
 }
 
 export default LikeButton;
