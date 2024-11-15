cbe7a84c77558cf1bc4b403d6fea0f78d892bf6d
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-15 16:55:17 +0100

    Iteration 4 solved

--- src/Components/Dice.jsx
+++ src/Components/Dice.jsx
@@ -4,23 +4,27 @@ import { useState } from "react";
 function getRandomFace () {
     const randomNumber = Math.ceil(Math.random()*6);
     return randomNumber;
+} //This works
+
+const diceFacesArray = [/* './src/assets/images/dice1.png', './src/assets/images/dice2.png', './src/assets/images/dice3.png', './src/assets/images/dice4.png', './src/assets/images/dice5.png', './src/assets/images/dice6.png' */];
+
+for(let i = 0; i < 6; i++) {
+    diceFacesArray.push(`./src/assets/images/dice${i+1}.png`);
 }
+console.log(diceFacesArray);
+
+
 
 function Dice() {
     const [diceFace, setDiceFace] = useState(0);
-    const diceFacesArray = ['./src/assets/images/dice1.png', './src/assets/images/dice2.png', './src/assets/images/dice3.png', './src/assets/images/dice4.png', './src/assets/images/dice5.png', './src/assets/images/dice6.png'];
- /*    for(let i = 0; i >= 6; i+1) {
-        diceFacesArray.push(`./src/assets/images/dice${i+1}.png`);
-    } */
-    console.log(diceFacesArray);
     return (
 
         <div className='dice'>
             <img onClick={
                 () => {
                     setDiceFace(0);
-                    setTimeout(setDiceFace(getRandomFace), 1000);
-                }} src={diceFace===0? './src/assets/images/dice-empty' : diceFacesArray[diceFace-1]} alt="dice" />
+                    setTimeout(() => setDiceFace(getRandomFace), 1000);
+                }} src={diceFace===0? './src/assets/images/dice-empty.png' : diceFacesArray[diceFace-1]} alt="dice" />
         </div>
     )
 }
